2c90fea3513c434ad3efde2ff7ab0778f7df2dbc
mmoramova <moramova@gmail.com>
2023-06-26 20:02:14 +0200

    added libraries from functions from subject

--- inc/minishell.h
+++ inc/minishell.h
@@ -3,12 +3,34 @@
 /*                                                        :::      ::::::::   */
 /*   minishell.h                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: josorteg <josorteg@student.42barcel>       +#+  +:+       +#+        */
+/*   By: mmoramov <mmoramov@student.42barcel>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/26 18:51:20 by josorteg          #+#    #+#             */
-/*   Updated: 2023/06/26 19:04:26 by josorteg         ###   ########.fr       */
+/*   Updated: 2023/06/26 20:02:05 by mmoramov         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <stdio.h>
-dsfdss
+#ifndef MINISHELL_H
+# define MINISHELL_H
+
+# include <dirent.h>
+# include <readline/readline.h>
+# include <readline/history.h>
+# include <signal.h>
+# include <stdio.h>
+# include <stdlib.h>
+# include <string.h>
+# include <sys/stat.h>
+# include <sys/wait.h>
+# include <sys/types.h>
+# include <unistd.h>
+# include <termios.h>
+//TODO INCLUDE LIBFT
+
+typedef struct s_x
+{
+	int xx;
+}	t_x;
+
+
+#endif
