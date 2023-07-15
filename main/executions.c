53ac7016d46a63434f86cda696936174fba8cd4c
mmoramova <moramova@gmail.com>
2023-07-15 21:04:48 +0200

    general command works

--- main/executions.c
+++ main/executions.c
@@ -6,7 +6,7 @@
 /*   By: mmoramov <mmoramov@student.42barcel>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/13 18:33:19 by josorteg          #+#    #+#             */
-/*   Updated: 2023/07/15 19:07:25 by mmoramov         ###   ########.fr       */
+/*   Updated: 2023/07/15 21:02:39 by mmoramov         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -45,7 +45,7 @@ void ft_exe_buitin(t_env *env,char **com)
 	//return (1);
 }
 
-int ft_lstppipes_count(t_ex *lst)
+int ft_cmds_count(t_ex *lst)
 {
 	int	len;
 
@@ -105,8 +105,9 @@ void	ft_singlecommand(t_ms *ms,char **env)
 
 int	ft_doublecommand(t_ms *ms,char **env)
 {
-	int	pipes[2];
-	int pids[2];
+	//nb of commands = 2
+	int	pipes[2]; //0
+	int pids[2]; //0 1
 
 	printf("Hi from doublecommand\n");
 	if (pipe(pipes) == -1)
@@ -173,8 +174,9 @@ int	ft_doublecommand(t_ms *ms,char **env)
 
 int	ft_tripplecommand(t_ms *ms,char **env)
 {
-	int pipes[2][2];
-	int pids[3];
+	//nb of commands = 3
+	int pipes[2][2]; // 0, 1
+	int pids[3];  //0,1 2
 
 	printf("Hi from tripplecommand\n");
 	if (pipe(pipes[0]) == -1)
@@ -274,6 +276,103 @@ int	ft_tripplecommand(t_ms *ms,char **env)
 	return (0);
 }
 
+int	ft_generalcommand(t_ms *ms,char **env)
+{
+	int	i;
+	int	l;
+	int k;
+	int m;
+
+	t_ex	*com;
+
+	com = ms->exe;
+	i = ft_cmds_count(ms->exe); //count the commands
+
+	int pipes[i-1][2];
+	int pids[i];
+
+	printf("Hi from generalcommand\n");
+
+	l = 0;
+	while (l < i-1)
+	{
+		printf("Opening pipe %d\n", l);
+		if (pipe(pipes[l]) == -1)
+			ft_exit(errno, strerror(errno), NULL);
+		l++;
+	}
+
+	l = 0;
+	while (l < i)
+	{
+		pids[l] = fork();
+		if (pids[l] == -1)
+			ft_exit(errno, strerror(errno), NULL);
+		if (pids[l] == 0) //child l
+		{
+			printf("I am in pid[%d] (child %d)\n", l, l+1);  //child 1 is pid[0]
+
+			printf("-------------------Command %s %s\n",com->command[0], com->command[1]);
+
+			if (i > 1 && l > 0)
+			{
+				printf("------------------------------i = %d, l=%d , dup2 stdin pipe[%d][0]\n", i, l, l-1);
+				dup2(pipes[l-1][0], STDIN_FILENO);
+			}
+
+			if (i > 1 && l < i-1)
+			{
+				printf("-------------------------------i = %d, l=%d , dup2 stdout pipe[%d][1]\n", i, l, l);
+				dup2(pipes[l][1], STDOUT_FILENO);
+			}
+
+			if (com->fd[0])
+			{
+				dup2(com->fd[0] ,STDIN_FILENO);
+				close(com->fd[0]);
+			}
+			if (com->fd[1])
+			{
+				dup2(com->fd[1] ,STDOUT_FILENO);
+				close(com->fd[1]);
+			}
+
+			m = 0;
+			while (m < i-1)
+			{
+				close(pipes[m][0]);
+				close(pipes[m][1]);
+				m++;
+			}
+
+			ft_execve_prepare(ms, env, l);
+			return (0);
+		}
+		if (com->next)
+			com = com->next;
+		l++;
+	}
+
+	//parent
+	k = 0;
+	while (k < i-1)
+	{
+		printf("Closing pipes[%d]\n", k);
+		close(pipes[k][0]);
+		close(pipes[k][1]);
+			k++;
+	}
+
+	k = 0;
+	while (k < i)
+	{
+		printf("waiting for pids[%d]\n", k);
+		waitpid(pids[k], NULL, 0);
+		k++;
+	}
+	return (0);
+}
+
 void	ft_execute(t_ms	*ms, char **env)
 {
 	printf("Isbultin %s: %d \n", ms->exe->command[0], ft_isbuiltin(ms->exe->command[0]));
@@ -286,12 +385,14 @@ void	ft_execute(t_ms	*ms, char **env)
 		//	close(ms->exe->fd[1]);
 	}
 	else*/
-	if (ft_lstppipes_count(ms->exe) == 1)
+	/*if (ft_cmds_count(ms->exe) == 1)
 		ft_singlecommand(ms, env);
-	else if (ft_lstppipes_count(ms->exe) == 2)
+	else if (ft_cmds_count(ms->exe) == 2)
 		ft_doublecommand(ms, env);
 	else
 		ft_tripplecommand(ms, env);
+	*/
+	ft_generalcommand(ms, env);
 }
 
 
